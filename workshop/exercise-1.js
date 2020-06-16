f16de165cb155ae715763821f4158c37609292f1
Jeffrey <jeffreyplacido@hotmail.com>
2020-06-16 15:28:06 -0400

    Q1

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -1,13 +1,3 @@
-// -------------------------------------------------------------
-// String Methods Exercises
-// ----------
-// note: need to have covered string methods for these exercises
-// -------------------------------------------------------------
-
-// Write to the console, the desired length of each of the following strings.
-// When this exercise is complete, it should write all of the answers to the console one after the other.
-
-// Given the following data to answer the questions.
 const smartGuy = 'Rick Sanchez';
 const rickSaying = 'wubba lubba dub dub';
 const grandson = 'Morty Antoine Smith';
@@ -30,22 +20,57 @@ const notCode = [
 
 // Q1.1
 // How many names does Morty have? (use grandson)
+const mortyNamesArray = grandson.split(' ');
+const numberOfNames = mortyNamesArray.length;
+console.log(`Morty has ${numberOfNames} names`);
 
 // Q1.2
 // Convert the alternateRicks string to an array. Console that array.
+const altRick = alternateRicks.split(" ,")
+console.log(altRick)
 
 // Q1.3
 // How many Ricks have been named in alternateRicks?
 
+
+let numRicks = 0;
+for (let x = 0; x < alternateRicks.length; x++) {
+  if (alternateRicks[x].includes('Ricks')) {
+    numRicks += 1;
+  }
+}
+console.log(`${numRicks} Ricks have been named`);
+
 // Q1.4
 // How many characters are there in rickSaying (without spaces)?
 
+let ricksayingArray = rickSaying.split('');
+let characters = 0;
+for (let x = 0; x < ricksayingArray.length; x++) {
+  if (ricksayingArray[x] !== ' ') {
+    characters += 1;
+  }
+}
+console.log(`rickSaying has ${characters} characters`);
+
 // Q1.4
 // Morty doesn't actually have a middle name. Console Morty's name without "Antoine".
+
+const newMorty = grandson.replace(' Antoine','');
+console.log(newMorty);
 
 // Q1.5
 // Decode and console. the secret!
 // To do so, you will need to remove all of the notCode words.
 
+let reveal = secret;
+for (let x = 0; x < notCode.length; x++) {
+  decodedMsg = decodedMsg.replace(notCode[x], '');
+}
+console.log(reveal);
+
 // Q1.6 (Stretch Goal)
 // Scary Terry is actually not an enemy. Remove him from the list and console only Rick's true enemies.
+
+let Villains = enemies.replace('Scary Terry, ', '');
+console.log(Villains);
