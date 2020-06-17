345a86be5e506467a44bceb1879bfe64ba87aeb9
Jeffrey <jeffreyplacido@hotmail.com>
2020-06-16 23:33:10 -0400

    Q12

--- workshop/exercise-12.js
+++ workshop/exercise-12.js
@@ -10,6 +10,23 @@
 
 // Write function here
 
+function calculateAverage(grades) {
+    let sum = grades.reduce((a, b) => a + b);
+    let average = Math.round(sum / grades.length);
+
+
+    if (average < 60) return 'F';
+    if (average < 70) return 'D';
+    if (average < 80) return 'C';
+    if (average < 90) return 'B';
+    if (average < 100) return 'A';
+}
 // Call the function with [48, 95, 65, 48, 59, 78, 72, 65]
 
+console.log(calculateAverage([76, 60, 83, 100, 78]));
+
+
 // Try with other values as well
+console.log(calculateAverage([63, 25, 37, 19, 76, 53]));
+console.log(calculateAverage([87, 93, 94, 91]));
+
